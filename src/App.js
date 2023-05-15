049778bfed7c0b0f78187596e415ba928d7c9820
Param Jansari <paramjansari0@gmail.com>
2023-05-14 21:19:41 -0400

    implemented openweather api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -11,15 +11,42 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import axios from 'axios';
 
 function App() {
+  const [cityName, setCityName] = useState('Toronto')
+  const [weatherData, setWeatherData] = useState()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(event.target.cityName.value)
+    setCityName(event.target.cityName.value)
+  }
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_OPEN_WEATHER_API, {
+      params: {
+        q: `${cityName}, CA`,
+        appid: 'ac354a64e71cf853731343f90fdcf94a',
+        units: 'metric'
+      }
+    })
+      .then(function (response) {
+        console.log(response.data);
+        setWeatherData(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [cityName])
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <form>
+            <form onSubmit={handleSubmit}>
               <FormControl isRequired>
                 <Input
                   name='cityName'
